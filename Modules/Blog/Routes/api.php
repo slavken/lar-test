a60f05b8f2a7d8354b13f175141103e972134470
Slava <slavken@outlook.com>
2021-03-25 15:45:13 +0300

    Refactoring

--- Modules/Blog/Routes/api.php
+++ Modules/Blog/Routes/api.php
@@ -3,7 +3,9 @@
 use App\UseCases\RouteModule;
 use Illuminate\Support\Facades\Route;
 use Modules\Blog\Http\Controllers\API\ArticleController;
-use Modules\Blog\Http\Controllers\API\UserArticleController;
+use Modules\Blog\Http\Controllers\API\{
+    UserArticleController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -20,15 +22,7 @@ Route::prefix('blog-module')->middleware('module:blog')->group(function () {
     new RouteModule();
 
     Route::prefix('me')->middleware('auth:api')->group(function () {
-        // apiResource/resource не работает из-за laravel-modules
-        // Target class [Modules\Blog\Http\Controllers\Modules\Blog\Http\Controllers\ArticleController] does not exist.
-        Route::prefix('articles')->group(function () {
-            Route::get('/', [UserArticleController::class, 'index']);
-            Route::get('/{id}', [UserArticleController::class, 'show']);
-            Route::post('/', [UserArticleController::class, 'store']);
-            Route::put('/{id}', [UserArticleController::class, 'update']);
-            Route::delete('/{id}', [UserArticleController::class, 'destroy']);
-        });
+        Route::apiResource('articles', UserArticleController::class);
     });
 
     Route::get('/articles', [ArticleController::class, 'index']);
